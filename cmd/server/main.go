f37cf8f6c1546627479fc84a4927d5da4a486858
Tim Yuan <yuangang@me.com>
2024-09-29 15:53:00 +0800

    test: add main test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,75 +17,70 @@ var (
 	Version string
 	Commit  string
 )
+
 func main() {
-    // 初始化日志
-    logger.InitLogger()
-    defer logger.Log.Sync()
+	initializeApp()
+	app := createApp()
+	routes.SetupRoutes(app)
+	startServer(app)
+}
 
-    // 初始化配置
-    config.InitConfig()
+func initializeApp() {
+	logger.InitLogger()
+	config.InitConfig()
+	db.InitDB()
+	cache.InitRedis()
+}
 
-    // 初始化数据库
-    db.InitDB()
-
-    // 初始化 Redis
-    cache.InitRedis()
-
-    // 初始化 Fiber
-    // 创建 Fiber 应用实例，配置日志
+func createApp() *fiber.App {
 	app := fiber.New(fiber.Config{
-		// 启用日志
 		EnablePrintRoutes: true,
-		// 自定义日志输出
-		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			code := fiber.StatusInternalServerError
-			if e, ok := err.(*fiber.Error); ok {
-				code = e.Code
-			}
-			logger.Log.Error("Request error",
-				zap.Int("status", code),
-				zap.String("method", c.Method()),
-				zap.String("path", c.Path()),
-				zap.String("ip", c.IP()),
-				zap.Duration("latency", c.Locals("latency").(time.Duration)),
-				zap.Error(err),
-			)
-			return c.Status(code).JSON(fiber.Map{
-				"error": err.Error(),
-			})
-		},
+		ErrorHandler: customErrorHandler,
 	})
-    // 添加请求时间中间件
-	app.Use(func(c *fiber.Ctx) error {
-		start := time.Now()
-		err := c.Next()
-		latency := time.Since(start)
-		c.Locals("latency", latency)
-		
-		// 记录成功的请求
-		if err == nil {
-			logger.Log.Info("Request processed",
-				zap.String("method", c.Method()),
-				zap.String("path", c.Path()),
-				zap.Int("status", c.Response().StatusCode()),
-				zap.Duration("latency", latency),
-				zap.String("ip", c.IP()),
-			)
-		}
-		return err
+	app.Use(requestTimeMiddleware)
+	return app
+}
+
+func customErrorHandler(c *fiber.Ctx, err error) error {
+	code := fiber.StatusInternalServerError
+	if e, ok := err.(*fiber.Error); ok {
+		code = e.Code
+	}
+	logger.Log.Error("Request error",
+		zap.Int("status", code),
+		zap.String("method", c.Method()),
+		zap.String("path", c.Path()),
+		zap.String("ip", c.IP()),
+		zap.Duration("latency", c.Locals("latency").(time.Duration)),
+		zap.Error(err),
+	)
+	return c.Status(code).JSON(fiber.Map{
+		"error": err.Error(),
 	})
+}
 
-    
-    routes.SetupRoutes(app)
+func requestTimeMiddleware(c *fiber.Ctx) error {
+	start := time.Now()
+	err := c.Next()
+	latency := time.Since(start)
+	c.Locals("latency", latency)
 
+	// 记录成功的请求
+	if err == nil {
+		logger.Log.Info("Request processed",
+			zap.String("method", c.Method()),
+			zap.String("path", c.Path()),
+			zap.Int("status", c.Response().StatusCode()),
+			zap.Duration("latency", latency),
+			zap.String("ip", c.IP()),
+		)
+	}
+	return err
+}
 
-    // 启动服务器
-		// 打印版本信息
-    logger.Log.Info(fmt.Sprintf("Starting go-fiber-api version %s (commit %s)", Version, Commit),
+func startServer(app *fiber.App) error {
+	logger.Log.Info(fmt.Sprintf("Starting go-fiber-api version %s (commit %s)", Version, Commit),
 		zap.Int("port", config.Cfg.Server.Port),
-	  )
-
-    if err := app.Listen(fmt.Sprintf(":%d", config.Cfg.Server.Port)); err != nil {
-        logger.Log.Fatal("Error starting server", zap.Error(err))
-    }
+	)
+	return app.Listen(fmt.Sprintf(":%d", config.Cfg.Server.Port))
 }
--- go.mod
+++ go.mod
@@ -5,15 +5,20 @@ go 1.22.5
 require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/prometheus/client_golang v1.20.4
 	github.com/spf13/viper v1.19.0
+	github.com/stretchr/testify v1.9.0
+	go.uber.org/zap v1.27.0
 	gorm.io/driver/postgres v1.5.9
 	gorm.io/gorm v1.25.12
+	gorm.io/plugin/dbresolver v1.5.3
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -32,7 +37,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/prometheus/client_golang v1.20.4 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.59.1 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
@@ -47,9 +52,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.56.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/sync v0.8.0 // indirect
@@ -58,5 +61,4 @@ require (
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/plugin/dbresolver v1.5.3 // indirect
 )
